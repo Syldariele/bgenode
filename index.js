b8e4d1c1a21f9f718be8103bb7dfaad308dc0938
Sitael <beru666@live.fr>
2020-03-19 16:58:35 +0100

    add acces path on index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("SERVEUR STARTED...");
+});
+
+app.get('/', (req, res) => {
+    console.log("acces to / path");
+    res.send("hello world");
 });
